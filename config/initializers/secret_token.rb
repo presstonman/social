9ac2017585bbfe38fc2aa205b59c8fbc29ccbf62
Hysen Speci <hysen.speci@gmail.com>
2015-03-27 16:25:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = 'c531da8774e2648bde25bd05e8a8f044e20538389e1146c9e8cc8f2c0528a32f852a1d35c26d12b0bb3d9f10223d3c2da20a18805268414455b659010a0a5843'
